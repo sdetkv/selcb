830ebcde91b1d97db4965c7d07f95647c4d3b202
kvg10 <sdetkv@gmail.com>
2020-03-08 17:01:34 -0600

    Add pages for CarFax .

---
+++ src/test/java/pages/carfax/Home.java
@@ -0,0 +1,17 @@
+package pages.carfax;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Home {
+
+    private By popularSearch= By.xpath("//div[@id='accordion']/div/ul");
+    private By listOfAutoMakers= By.xpath("//div[@id='accordion']/div//a");
+
+
+
+
+
+
+
+}
